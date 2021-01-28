3b1e672df9a4f39b4026ae65593d70451242ef52
Jonathan Correa <jcorrea@intexus.com.co>
2021-01-28 16:53:43 -0500

    Revert "Revert "Revert "Se implementa swagger"""
    This reverts commit 580b581f2d72a24a2a64c4f0fc2a3b1bbabf2d50.

--- MVCMongoDB/Startup.cs
+++ MVCMongoDB/Startup.cs
@@ -1,13 +1,18 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Microsoft.OpenApi.Models;
 using MVCMongoDB.Models;
 using MVCMongoDB.Services;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace MVCMongoDB
 {
@@ -23,11 +28,6 @@ namespace MVCMongoDB
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors(c =>
-            {
-                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin());
-            });
-
             services.Configure<LogSettings>
                 (Configuration.GetSection(nameof(LogSettings)));
 
@@ -37,17 +37,11 @@ namespace MVCMongoDB
             services.AddSingleton<LogService>();
 
             services.AddControllers();
-
-            AddSwagger(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseCors(
-                option => option.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
-            );
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -62,35 +56,6 @@ namespace MVCMongoDB
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-            });
-
-            //Enable Swagger & Swagger UI
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                string swaggerJsonBasePath = string.IsNullOrWhiteSpace(c.RoutePrefix) ? "." : "..";
-                c.SwaggerEndpoint($"{swaggerJsonBasePath}/swagger/v1/swagger.json", "User Documentation API");
-            });
-        }
-
-        private void AddSwagger(IServiceCollection services)
-        {
-            services.AddSwaggerGen(options =>
-            {
-                var groupName = "v1";
-
-                options.SwaggerDoc(groupName, new OpenApiInfo
-                {
-                    Title = "Documentación API",
-                    Version = "v1.0",
-                    Description = "Documentación API",
-                    Contact = new OpenApiContact
-                    {
-                        Name = "Intexus",
-                        Email = "mobile.development@veriddica.com",
-                        Url = new Uri("http://intexus.com.co/"),
-                    }
-                });
             });
         }
     }
